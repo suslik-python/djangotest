0235ac33bffb69a0db082a7985acb8437758b68d
suslik <tlgnvsultan@gmail.com>
2025-12-22 19:22:42 +0600

    Revert "проект добавлен!"
    This reverts commit c4bcca173a631c18048c0c19dd1a5c765a83d508.

--- django1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from core.views import mainVeiew, contacts_page
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', mainVeiew),
-    path('contacts/', contacts_page)
-]
